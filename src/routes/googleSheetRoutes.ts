465997c417a0413085f3b9556af95768af70a141
sudhanshu2070 <sssallen2016@gmail.com>
2025-03-23 23:37:04 +0530

    Google sheet getting 0.3

--- src/routes/googleSheetRoutes.ts
+++ src/routes/googleSheetRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { saveFormData} from '../controllers/googleSheetController';
+import { getFormDataWhatsApp, saveFormData} from '../controllers/googleSheetController';
 import { getFormData } from '../controllers/googleSheetController';
 
 const router = express.Router();
 
 router.post('/saveUserGoogleSheet', saveFormData);//For saving the data to google sheets
 router.get('/getUserGoogleSheet', getFormData);
-router.get('/getUserGoogleSheetWhatsApp', getFormData);
+router.get('/getUserGoogleSheetWhatsApp', getFormDataWhatsApp);
 
 export default router;
